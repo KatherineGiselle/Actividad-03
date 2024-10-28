b53d877a9b0a34e68f585ee13c2e25cf4f2387a5
Katherine Giselle Diaz Morales <167901582+KatherineGiselle@users.noreply.github.com>
2024-10-28 14:13:16 -0700

    Create 31.pl

---
+++ 31.pl
@@ -0,0 +1,24 @@
+Encabezado
+% C# solucion / Python
+
+
+%------------------------------------------------------------------------------
+% Módulo:       50_programas
+% Propósito:    Elaborar 50 programas en Prolog
+% 
+% Autor:        Diaz Morales Katherine Giselle
+% Fecha:        21 de octubre de 2024
+%
+% Descripción: 
+% Elaborar 50 programas en Prolog, usar templete.
+
+--------------------------------------------------------------------------------------
+
+
+- - - - -  PROLOG - - - - -
+
+31. Determinar el máximo común divisor de dos números positivos.
+
+% Calcula el máximo común divisor usando el algoritmo de Euclides.
+gcd(X, 0, X) :- X > 0.
+gcd(X, Y, G) :- Y > 0, R is X mod Y, gcd(Y, R, G).
