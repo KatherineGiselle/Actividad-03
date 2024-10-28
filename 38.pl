87f76d7d088955847dbbb870c667115106e7fb4b
Katherine Giselle Diaz Morales <167901582+KatherineGiselle@users.noreply.github.com>
2024-10-28 14:19:56 -0700

    Create 38.pl

---
+++ 38.pl
@@ -0,0 +1,23 @@
+Encabezado
+% C# solucion / Python
+
+
+%------------------------------------------------------------------------------
+% Módulo:       50_programas
+% Propósito:    Elaborar 50 programas en Prolog
+% 
+% Autor:        Diaz Morales Katherine Giselle
+% Fecha:        21 de octubre de 2024
+%
+% Descripción: 
+% Elaborar 50 programas en Prolog, usar templete.
+
+--------------------------------------------------------------------------------------
+
+
+- - - - -  PROLOG - - - - -
+
+38. Lista de números primos en un rango.
+
+% Genera una lista de números primos en un rango dado.
+prime_list(Low, High, Primes) :- findall(P, (between(Low, High, P), is_prime(P)), Primes).
