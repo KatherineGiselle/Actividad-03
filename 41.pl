a6d0b637ad9e9512d1bbb59371145f96fc5654e9
Katherine Giselle Diaz Morales <167901582+KatherineGiselle@users.noreply.github.com>
2024-10-28 14:26:17 -0700

    Create 41.pl

---
+++ 41.pl
@@ -0,0 +1,25 @@
+Encabezado
+% C# solucion / Python
+
+
+%------------------------------------------------------------------------------
+% Módulo:       50_programas
+% Propósito:    Elaborar 50 programas en Prolog
+% 
+% Autor:        Diaz Morales Katherine Giselle
+% Fecha:        21 de octubre de 2024
+%
+% Descripción: 
+% Elaborar 50 programas en Prolog, usar templete.
+
+--------------------------------------------------------------------------------------
+
+
+- - - - -  PROLOG - - - - -
+
+41. Tabla de verdad para expresiones lógicas.
+
+% Genera una tabla de verdad para una expresión lógica en dos variables.
+% A y B son las variables lógicas y Expr es la expresión lógica que se evalúa.
+table(A, B, Expr) :- write(A), write(' '), write(B), write(' '), call(Expr), nl, fail.
+table(_, _, _).
