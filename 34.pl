b601a8cb53e6552e0c83ef0234581b7f0c79b1dc
Katherine Giselle Diaz Morales <167901582+KatherineGiselle@users.noreply.github.com>
2024-10-28 14:16:19 -0700

    Create 34.pl

---
+++ 34.pl
@@ -0,0 +1,30 @@
+Encabezado
+% C# solucion / Python
+
+
+%------------------------------------------------------------------------------
+% Módulo:       50_programas
+% Propósito:    Elaborar 50 programas en Prolog
+% 
+% Autor:        Diaz Morales Katherine Giselle
+% Fecha:        21 de octubre de 2024
+%
+% Descripción: 
+% Elaborar 50 programas en Prolog, usar templete.
+
+--------------------------------------------------------------------------------------
+
+
+- - - - -  PROLOG - - - - -
+
+34. Determinar los factores primos de un número entero positivo.
+
+% Encuentra los factores primos de un número entero positivo.
+prime_factors(N, L) :- N > 1, prime_factors(N, 2, L).
+
+prime_factors(1, _, []).
+prime_factors(N, F, [F|L]) :- N > 1, 0 is N mod F, N1 is N // F, prime_factors(N1, F, L).
+prime_factors(N, F, L) :- N > 1, F * F < N, next_factor(F, F1), prime_factors(N, F1, L).
+
+next_factor(2, 3).
+next_factor(F, F1) :- F > 2, F1 is F + 2.
