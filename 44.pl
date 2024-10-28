5b85e824a8ad28c731a0b06242812b14ad8c89f6
Katherine Giselle Diaz Morales <167901582+KatherineGiselle@users.noreply.github.com>
2024-10-28 14:36:37 -0700

    Create 44.pl

---
+++ 44.pl
@@ -0,0 +1,29 @@
+Encabezado
+% C# solucion / Python
+
+
+%------------------------------------------------------------------------------
+% Módulo:       50_programas
+% Propósito:    Elaborar 50 programas en Prolog
+% 
+% Autor:        Diaz Morales Katherine Giselle
+% Fecha:        21 de octubre de 2024
+%
+% Descripción: 
+% Elaborar 50 programas en Prolog, usar templete.
+
+--------------------------------------------------------------------------------------
+
+
+- - - - -  PROLOG - - - - -
+
+44. Código Huffman.
+
+% Genera un árbol de Huffman dado un conjunto de frecuencias.
+% https://es.wikipedia.org/wiki/Codificación_Huffman
+% El algoritmo de Huffman se utiliza para la compresión de datos, construyendo un árbol binario óptimo para la codificación.
+huffman(Fs, Hs) :- length(Fs, Len), Len > 1, sort(2, @=<, Fs, Sorted), huffman_tree(Sorted, Hs).
+
+% Caso base para construir el árbol de Huffman.
+huffman_tree([fr(X, _) | []], hc(X, '0')).
+huffman_tree([fr(_, _) | [fr(_, _) | _]], _).
