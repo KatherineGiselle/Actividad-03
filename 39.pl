c88c023eb56ed92982c547639c0936435005dbd8
Katherine Giselle Diaz Morales <167901582+KatherineGiselle@users.noreply.github.com>
2024-10-28 14:23:49 -0700

    Create 39.pl

---
+++ 39.pl
@@ -0,0 +1,24 @@
+Encabezado
+% C# solucion / Python
+
+
+%------------------------------------------------------------------------------
+% Módulo:       50_programas
+% Propósito:    Elaborar 50 programas en Prolog
+% 
+% Autor:        Diaz Morales Katherine Giselle
+% Fecha:        21 de octubre de 2024
+%
+% Descripción: 
+% Elaborar 50 programas en Prolog, usar templete.
+
+--------------------------------------------------------------------------------------
+
+
+- - - - -  PROLOG - - - - -
+
+39. Conjetura de Goldbach.
+
+% Encuentra dos números primos que sumen un número par dado según la conjetura de Goldbach.
+% https://es.wikipedia.org/wiki/Conjetura_de_Goldbach
+goldbach(N, [P1, P2]) :- N > 2, N mod 2 =:= 0, prime_list(2, N, Primes), member(P1, Primes), P2 is N - P1, is_prime(P2).
