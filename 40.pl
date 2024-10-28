387e0e0e387b9cb0b0f0648a87178f0d62c163c2
Katherine Giselle Diaz Morales <167901582+KatherineGiselle@users.noreply.github.com>
2024-10-28 14:25:42 -0700

    Create 40.pl

---
+++ 40.pl
@@ -0,0 +1,23 @@
+Encabezado
+% C# solucion / Python
+
+
+%------------------------------------------------------------------------------
+% Módulo:       50_programas
+% Propósito:    Elaborar 50 programas en Prolog
+% 
+% Autor:        Diaz Morales Katherine Giselle
+% Fecha:        21 de octubre de 2024
+%
+% Descripción: 
+% Elaborar 50 programas en Prolog, usar templete.
+
+--------------------------------------------------------------------------------------
+
+
+- - - - -  PROLOG - - - - -
+
+40. Lista de composiciones de Goldbach para un rango de números.
+
+% Encuentra las composiciones de Goldbach para todos los números pares dentro de un rango.
+goldbach_list(Low, High, L) :- findall([N, P1, P2], (between(Low, High, N), N mod 2 =:= 0, goldbach(N, [P1, P2])), L).
